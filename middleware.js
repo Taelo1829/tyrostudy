71c18b0fcc178719411d08b707bbdc7c41f27108
Taelo <taelo@izon.group>
2026-05-04 08:30:12 +0200

    fixed middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,20 @@
-// middleware.ts (root of project)
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const isAuthenticated = request.cookies.get('token') // or your auth check
-  const isLoginPage = request.nextUrl.pathname === '/login'
+  const isAuthenticated = request.cookies.get('token')
+  const { pathname } = request.nextUrl
+
+  const isLoginPage = pathname === '/login'
 
   if (!isAuthenticated && !isLoginPage) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|manifest.json|icons|images).*)',
+  ],
 }
