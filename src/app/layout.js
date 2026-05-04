3a036ee0e2416993187b7dd774d1c1394904c819
Taelo <taelo@izon.group>
2026-05-04 08:23:41 +0200

    worked on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
         <Provider>
